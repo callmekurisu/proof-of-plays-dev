eabac8f55865bf06d51e96e98cd986c06624d8bb
kurisu <callmekurisu@gmail.com>
2019-08-08 20:08:25 -0400

    added sample config file

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+/* Login to mongo with admin
+   > mongo --port 27017 -u "user" -p "password" --authenticationDatabase "admin"
+   Create and switch to new database
+   > use "db name"
+   Create database owner with
+   > db.createUser({user: "user" , pwd: "kurisu", roles: [  "dbOwner" ]})
+*/
+
+module.exports = {
+    mongoURI:
+    'mongodb://user:user@localhost:27017/plays-dev',
+    superSecret: 'secret',
+    DEV_CLIENT: "http://localhost:3000"
+  }
